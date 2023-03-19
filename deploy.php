81ee59203258b895d12c9a40b6be3f7d43d91292
amirali <amirali10281@gmail.com>
2023-03-19 16:15:25 +0330

    fix3

--- deploy.php
+++ deploy.php
@@ -10,7 +10,7 @@ set('application', 'mti_main_core');
 set('repository', 'git@github.com:MTI-IR/core_main_back.git');
 set('php_fpm_version', '8.0');
 
-host('prod')
+host('production')
     ->set('remote_user', 'root')
     ->set('hostname', 'mti_main_core.ir')
     ->set('deploy_path', '/var/www/{{hostname}}');
@@ -22,15 +22,9 @@ task('deploy', [
     'artisan:view:cache',
     'artisan:config:cache',
     'artisan:migrate',
-    'npm:install',
-    'npm:run:prod',
     'deploy:publish',
     'php-fpm:reload',
 ]);
 
-task('npm:run:prod', function () {
-    cd('{{release_or_current_path}}');
-    run('npm run prod');
-});
 
 after('deploy:failed', 'deploy:unlock');
