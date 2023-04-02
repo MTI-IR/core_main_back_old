48a7cb935491d90fc67b017822b24719866e0709
amirali <79888220+amirali10281@users.noreply.github.com>
2023-04-02 15:45:15 +0330

    Revert "Revert "add middlewares""

---
+++ app/Http/Middleware/PermissionMiddleware.php
@@ -0,0 +1,53 @@
+<?php
+
+
+use App\Models\User;
+use Closure;
+use Spatie\Permission\Models\Permission;
+use Throwable;
+
+class PermissionMiddleware
+{
+    public function handle($request, Closure $next, $permission, $guard = null)
+    {
+        $user_id = $request->get('user')->id;
+        $user = User::findOrFail($user_id);
+        $permissions = is_array($permission)
+            ? $permission
+            : explode('|', $permission);
+
+
+        try {
+            if ($user->hasPermissionTo('super-admin'))
+                return $next($request);
+            foreach ($permissions as $permission) {
+                try {
+                    Permission::findByName($permission, 'admin');
+                    if ($user->hasPermissionTo($permission)) return $next($request);
+                } catch (Throwable $e) {
+                    Permission::create(["name" => $permission, 'guard_name' => 'admin']);
+                    return  response()->json([
+                        "message" => "You have the permission :: " . $permission,
+                        "status" => "403",
+                    ], 403);
+                }
+            }
+        } catch (Throwable $e) {
+            try {
+                Permission::findByName($permission, 'admin');
+                if ($user->hasPermissionTo($permission)) return $next($request);
+            } catch (Throwable $e) {
+                Permission::create(["name" => $permission, 'guard_name' => 'admin']);
+                return  response()->json([
+                    "message" => "You have the permission :: " . $permission,
+                    "status" => "403",
+                ], 403);
+            }
+        }
+
+        return  response()->json([
+            "message" => "You have the permission : " . $permission,
+            "status" => "403",
+        ], 403);
+    }
+}
